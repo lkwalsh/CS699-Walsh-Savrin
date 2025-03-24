7d6d43482cb3cdc6fb8dab088e186400d298c180
Lauren Savrin <lsav1818@gmail.com>
2025-03-24 18:19:42 -0400

    Updated preprocessing_code.R with latest changes

--- preprocessing_code.R
+++ preprocessing_code.R
@@ -7,47 +7,48 @@ library(dplyr)
 library(tidyr)
 library(caret)
 
-# Load dataset
+###Load dataset###
 file<-read.csv("C:/Users/lsav1/Downloads/project_data.csv")
 data <- file
 dim(data)
 
-# Check for missing values
+###Check for missing values###
 sum(is.na(data)) # Number of missing values in the dataset
 sapply(data, function(x) sum(is.na(x))) # Missing values per column
 
-# Remove columns where more than 50% of rows are NA
+###Remove columns where more than 50% of rows are NA###
 threshold <- 0.5 * nrow(data)
 df_col_trim <- data[, colSums(is.na(data)) <= threshold]
 dim(df_col_trim)
 sapply(df_col_trim, function(x) sum(is.na(x)))
 
-# Replace remaining NA values with the median (imputation)
+###Replace remaining NA values with the median (imputation)###
 df_imputation <- df_col_trim %>%
   mutate(across(where(is.numeric), 
                 ~ replace(., is.na(.), median(., na.rm = TRUE))))
 sapply(df_imputation, function(x) sum(is.na(x)))
 dim (df_imputation)
-# Check for duplicates
+
+###Check for duplicates###
 df_unique <- unique(df_imputation)
 dim(df_unique)
 
-# Convert class to a binary numeric (1 = Yes, 0 = No)
+###Convert class to a binary numeric (1 = Yes, 0 = No)###
 df_imputation$Class <- ifelse(df_imputation$Class == "Yes", 1, 0)
 
-# Remove non numeric columns
+###Remove non numeric columns###
 df_numeric <- df_imputation %>% dplyr::select(-RT, -SERIALNO)
 dim(df_numeric)
 colnames(df_numeric)
 
-# Make sure zero variance columns are removed
+###Make sure zero variance columns are removed###
 variances <- apply(df_numeric, 2, var, na.rm = TRUE)
 zero_variance_cols <- names(variances)[variances == 0]
 zero_variance_cols
 df_numeric_no_zero <- df_numeric[, variances > 0]
 dim(df_numeric_no_zero)
 
-# Check for low variance columns and remove those as well
+###Check for low variance columns and remove those as well###
 low_variances <- apply(df_numeric_no_zero, 2, var, na.rm = TRUE)
 threshold <- 0.01  # Set threshold for low variance
 low_var_cols <- names(low_variances)[low_variances < threshold]
@@ -60,16 +61,16 @@ colnames(df_numeric_no_var)
 # (1) First ensures zero-variance columns are removed
 # (2) Second filters out low-variance columns
 
-# Check for highly correlated columns
+###Check for highly correlated columns###
 cor_matrix <- cor(df_numeric_no_var)
 print(cor_matrix)
 high_cor_matrix <- cor_matrix > 0.5
 
-# Identify correlated pairs
+###Identify correlated pairs###
 high_cor_pairs <- which(high_cor_matrix, arr.ind = TRUE)
 high_cor_pairs <- high_cor_pairs[high_cor_pairs[,1] != high_cor_pairs[,2], ] # Remove diagonal
 
-# Review highly correlated columns
+###Review highly correlated columns###
 high_cor_df <- data.frame(
   var1 = rownames(cor_matrix)[high_cor_pairs[, 1]],
   var2 = colnames(cor_matrix)[high_cor_pairs[, 2]],
@@ -78,12 +79,12 @@ high_cor_df <- data.frame(
 
 high_cor_df
 
-# Remove highly correlated columns
+###Remove highly correlated columns###
 df_numeric_no_cor <- df_numeric_no_var %>% dplyr::select(-RAC1P, -RAC2P, -RACNUM, -RACSOR, -CIT, -NATIVITY, -MSP, -WAOB, -WAGP, -PERNP, -OC, -WRK, -NWLA, -NWAB, -NWLK)
 dim(df_numeric_no_cor)
 print(colnames(df_numeric_no_cor))
 
-#Identify outliers the boxplot
+###Identify outliers the boxplot###
 
 summary(df_numeric_no_cor$SPORDER)
 table(boxplot(df_numeric_no_cor$SPORDER, plot = FALSE)$out)
@@ -237,8 +238,9 @@ sum(table(boxplot(df_numeric_no_cor$RAC3P, plot = FALSE)$out))
 #850 total. looks like anything 4 or above
 
 
-#Handling outliers
+###Handling outliers###
 df_outliers<-df_numeric_no_cor
+
 #SPORDER
 df_outliers$SPORDER <- log(df_outliers$SPORDER)
 
@@ -270,14 +272,10 @@ summary(df_outliers$RETP)
 sum(table(boxplot(df_outliers$RETP, plot = FALSE)$out))
 
 #SEMP
-df_outliers$SEMP <- log(df_outliers$SEMP+1)
+df_outliers$SEMP <- sign(df_numeric_no_cor$SEMP) * abs(df_numeric_no_cor$SEMP)^(1/4)
 
 summary(df_outliers$SEMP)
 sum(table(boxplot(df_outliers$SEMP, plot = FALSE)$out))
-
-problematic_rows <- df_numeric_no_cor[df_numeric_no_cor$SEMP < -1 | is.na(df_numeric_no_cor$SEMP) | is.infinite(df_numeric_no_cor$SEMP), ]
-print(problematic_rows)
-
 
 #SSIP
 df_outliers$SSIP <- log(df_outliers$SSIP+1)
@@ -291,10 +289,20 @@ df_outliers$SSP <- log(df_outliers$SSP+1)
 summary(df_outliers$SSP)
 sum(table(boxplot(df_outliers$SSP, plot = FALSE)$out))
 
-##INSERT OUTLIER TABLES HERE
+#PINCP
+df_outliers$PINCP <- sign(df_outliers$PINCP) * abs(df_outliers$PINCP)^(1/4)
 
-#Split? 
+summary(df_outliers$PINCP)
+sum(table(boxplot(df_outliers$PINCP, plot = FALSE)$out))
 
+#INTP
+df_outliers$INTP <- sign(df_outliers$INTP) * abs(df_outliers$INTP)^(1/4)
+
+summary(df_outliers$INTP)
+sum(table(boxplot(df_outliers$INTP, plot = FALSE)$out))
+
+
+###Split### 
 set.seed(31)
 library(rsample)
 split <- initial_split(data, prop = 0.60, strata = Class)
@@ -304,6 +312,5 @@ dim(training)
 dim(test)
 table(training$Class)
 
-system("git add preprocessing_code.R")
 system('git commit -m "Updated preprocessing_code.R with latest changes"')
 system("git push origin main")
